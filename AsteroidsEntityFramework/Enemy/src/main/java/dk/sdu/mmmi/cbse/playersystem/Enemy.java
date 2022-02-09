48e2b45ddb7eab4f92c78fbf066764e8ca782424
Valon Morina <valon.morina301@hotmail.com>
2022-02-09 17:49:18 +0100

    EnemyClass

---
+++ AsteroidsEntityFramework/Enemy/src/main/java/dk/sdu/mmmi/cbse/playersystem/Enemy.java
@@ -0,0 +1,13 @@
+package dk.sdu.mmmi.cbse.playersystem;
+
+import dk.sdu.mmmi.cbse.common.data.Entity;
+
+import dk.sdu.mmmi.cbse.common.data.entityparts.MovingPart;
+
+/**
+ *
+ * @author corfixen
+ */
+public class Enemy extends Entity {
+
+}
