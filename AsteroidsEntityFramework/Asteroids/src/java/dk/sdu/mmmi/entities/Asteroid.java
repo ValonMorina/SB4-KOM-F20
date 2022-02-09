810fce3c1621eca33287f11c4363725333a586df
Valon Morina <valon.morina301@hotmail.com>
2022-02-09 17:56:20 +0100

    Asteroids

---
+++ AsteroidsEntityFramework/Asteroids/src/java/dk/sdu/mmmi/entities/Asteroid.java
@@ -0,0 +1,25 @@
+package dk.sdu.mmmi.entities;
+
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.World;
+import dk.sdu.mmmi.cbse.common.services.IEntityProcessingService;
+import dk.sdu.mmmi.cbse.common.services.IGamePluginService;
+
+public class Asteroid implements IGamePluginService, IEntityProcessingService {
+
+
+    @Override
+    public void start(GameData gameData, World world) {
+
+    }
+
+    @Override
+    public void stop(GameData gameData, World world) {
+
+    }
+
+    @Override
+    public void process(GameData gameData, World world) {
+
+    }
+}
